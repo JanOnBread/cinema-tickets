447aeb72063aa63bb0185b80982f08bdabb58675
JanOnBread <jcworkandschool@gmail.com>
2026-04-14 15:57:47 +0100

    chore: remove comments

--- src/pairtest/helpers/CalculateCostsAndSeats.js
+++ src/pairtest/helpers/CalculateCostsAndSeats.js
@@ -1,24 +1,4 @@
-//   return {
-//     adult: adultCount,
-//     child: childCount,
-//     infant: infantCount,
-//   };
-// }
-
 import logger from "../../utils/logger";
-
-// |   Ticket Type    |     Price   |
-
-// | ---------------- | ----------- |
-
-// |    INFANT        |    £0       |
-
-// |    CHILD         |    £15     |
-
-// |    ADULT         |    £25      |
-
-//   const totalPrice = calculateTotalCost(requestedTickets);
-//   const numSeats = calculateNumberOfSeats(requestedTickets);
 
 export default function calculateCostsAndSeats(requestedTickets) {
   logger.debug("In calculateCostsAndSeats()");
